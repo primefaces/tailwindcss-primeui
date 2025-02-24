306b8ca83395eb290ea2213a34efe34ba81bffa4
Mert Sincan <sincan.mert@gmail.com>
2025-02-24 09:47:07 +0000

    refactor: update package structure and paths for v3 and v4 compatibility

--- packages/tailwindcss-primeui/scripts/postbuild.mjs
+++ packages/tailwindcss-primeui/scripts/postbuild.mjs
@@ -4,7 +4,7 @@ import { clearPackageJson, resolvePath } from '../../../scripts/build-helper.mjs
 
 const { __dirname, __workspace, OUTPUT_DIR, INPUT_DIR } = resolvePath(import.meta.url);
 
-fs.copySync(path.resolve(__dirname, `../${INPUT_DIR}/css`), `${OUTPUT_DIR}/css`);
+fs.copySync(path.resolve(__dirname, `../${INPUT_DIR}/v4`), `${OUTPUT_DIR}/v4`);
 fs.copySync(path.resolve(__dirname, '../package.json'), `${OUTPUT_DIR}/package.json`);
 fs.copySync(path.resolve(__dirname, '../README.md'), `${OUTPUT_DIR}/README.md`);
 fs.copySync(path.resolve(__workspace, './LICENSE'), `${OUTPUT_DIR}/LICENSE`);
--- packages/tailwindcss-primeui/tsup.config.ts
+++ packages/tailwindcss-primeui/tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsup';
 export default defineConfig([
     {
         entry: {
-            'js/index': 'src/js/index.ts'
+            'v3/index': 'src/v3/index.ts'
         },
         format: ['esm', 'cjs', 'iife'],
         outDir: 'dist',
